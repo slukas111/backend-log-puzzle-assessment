870d1d30bec2941ee9f2b4ff9cc28e9af31ee08e
slukas111 <sashamati@gmail.com>
2020-04-05 10:26:48 -0700

    function read sort url

--- logpuzzle.py
+++ logpuzzle.py
@@ -29,47 +29,61 @@ def read_urls(filename):
     Screens out duplicate urls and returns the urls sorted into
     increasing order."""
     # +++your code here+++
-    pass
+    with open(filename, 'r') as file:
+        file.close()
+
+    url_list = []
+    for line in file:
+        match = re.search('puzzle', line)
+        if match:
+            url = re.searcg(r'\S+puzzle|S+.jpg', line)
+            if url:
+                url_list.append(url.group())
+    sorted_url_list = sorted(list(set(url_list)), key=lambda url: url[-8:-4])
+    print("list sorted")
+    return sorted_url_list
 
 
-def download_images(img_urls, dest_dir):
-    """Given the urls already in the correct order, downloads
-    each image into the given directory.
-    Gives the images local filenames img0, img1, and so on.
-    Creates an index.html in the directory
-    with an img tag to show each local image file.
-    Creates the directory if necessary.
-    """
-    # +++your code here+++
-    pass
 
 
-def create_parser():
-    """Create an argument parser object"""
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-d', '--todir',  help='destination directory for downloaded images')
-    parser.add_argument('logfile', help='apache logfile to extract urls from')
-
-    return parser
-
-
-def main(args):
-    """Parse args, scan for urls, get images from urls"""
-    parser = create_parser()
-
-    if not args:
-        parser.print_usage()
-        sys.exit(1)
-
-    parsed_args = parser.parse_args(args)
-
-    img_urls = read_urls(parsed_args.logfile)
-
-    if parsed_args.todir:
-        download_images(img_urls, parsed_args.todir)
-    else:
-        print('\n'.join(img_urls))
+# def download_images(img_urls, dest_dir):
+#     """Given the urls already in the correct order, downloads
+#     each image into the given directory.
+#     Gives the images local filenames img0, img1, and so on.
+#     Creates an index.html in the directory
+#     with an img tag to show each local image file.
+#     Creates the directory if necessary.
+#     """
+#     # +++your code here+++
+#     pass
 
 
-if __name__ == '__main__':
-    main(sys.argv[1:])
+# def create_parser():
+#     """Create an argument parser object"""
+#     parser = argparse.ArgumentParser()
+#     parser.add_argument('-d', '--todir',  help='destination directory for downloaded images')
+#     parser.add_argument('logfile', help='apache logfile to extract urls from')
+
+#     return parser
+
+
+# def main(args):
+#     """Parse args, scan for urls, get images from urls"""
+#     parser = create_parser()
+
+#     if not args:
+#         parser.print_usage()
+#         sys.exit(1)
+
+#     parsed_args = parser.parse_args(args)
+
+#     img_urls = read_urls(parsed_args.logfile)
+
+#     if parsed_args.todir:
+#         download_images(img_urls, parsed_args.todir)
+#     else:
+#         print('\n'.join(img_urls))
+
+
+# if __name__ == '__main__':
+#     main(sys.argv[1:])
